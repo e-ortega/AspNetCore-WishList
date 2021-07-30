f58e9b45bf12a37277c19df4e183a7662d0ddc6e
Esteban Ortega <e-ortega@users.noreply.github.com>
2021-07-29 22:43:35 -0600

    Update HomeController.cs
    Completed module

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -10,11 +10,11 @@ namespace WishList.Controllers
     {
         public IActionResult Index()
         {
-            return View("/Home/Index");
+            return View("Index");
         }
         public IActionResult Error()
         {
-            return View("/Shared/Error");
+            return View("Error");
         }
     }
 }
